8d51cf1db41101201d3809529106894dd644d517
Jason E. Aten <j.e.aten@gmail.com>
2012-02-06 02:52:37 -0600

    transfer to viewable

---
+++ l3ts_server.h
@@ -0,0 +1,14 @@
+//
+// Copyright (C) 2011 Jason E. Aten. All rights reserved.
+//
+#ifndef L3L3TS_SERVER_H
+#define L3L3TS_SERVER_H
+
+
+#include "l3path.h"
+
+void ptr_to_uuid_pbsn(void* ptr, l3path& pbsn);
+
+
+
+#endif /* L3L3TS_SERVER_H */
